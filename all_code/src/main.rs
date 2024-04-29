6e53656837fe39bc4f5265ba072d55627de8a8b1
YourAngel64 <angelyadiel8776@gmail.com>
2024-04-28 19:04:54 -0500

    First commit for chapters 1-3

--- all_code/src/main.rs
+++ all_code/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+
+enum FileState{
+    Open,
+    Closed,
+}
+
+#[derive(Debug)]
+struct File{
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for FileState{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        match *self{
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
+impl File{
+    fn new(name: &str) -> File{
+        File{
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+}
+
+fn main(){
+    let mut f1 = File::new("f1.txt");
+    f1.data = vec![100, 12, 45, 64];
+
+    println!("{:?}", f1);
+    println!("{}", f1);
 }
---
+++ all_code/target/debug/deps/all_code-c67f36cd2957723e.d
@@ -0,0 +1,5 @@
+/home/Enten/coding/Rust/Rust_in_action_learn/all_code/target/debug/deps/liball_code-c67f36cd2957723e.rmeta: src/main.rs
+
+/home/Enten/coding/Rust/Rust_in_action_learn/all_code/target/debug/deps/all_code-c67f36cd2957723e.d: src/main.rs
+
+src/main.rs:
