68fb6f7527ce20fca4500fdb23a1fb2799602bf1
YourAngel64 <angelyadiel8776@gmail.com>
2024-05-02 21:53:03 -0500

    started cpu emulator

---
+++ all_code/target/debug/deps/all_code-6552e18a725b3311.d
@@ -0,0 +1,5 @@
+/home/Enten/coding/Rust/Rust_in_action_learn/all_code/target/debug/deps/all_code-6552e18a725b3311: src/main.rs
+
+/home/Enten/coding/Rust/Rust_in_action_learn/all_code/target/debug/deps/all_code-6552e18a725b3311.d: src/main.rs
+
+src/main.rs:
